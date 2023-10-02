8cc98cdc3d0944e951bd139163b17992d5d82106
Marvis Ighedosa <dosamarvis@gmail.com>
2023-10-02 15:31:52 +0100

    Initial Commit

---
+++ MetflixApp/MetflixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetflixAppApp.swift
+//  MetflixApp
+//
+//  Created by Marvis Ighedosa on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetflixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
